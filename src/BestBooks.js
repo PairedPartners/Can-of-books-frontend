217f627a723e7a5261122a53e61387de598a2da7
nickmullaney <nick.mullaney@gmail.com>
2023-02-15 19:40:47 -0800

    post and delete now work

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import BookCarousel from './BookCarousel';
-import { Carousel } from 'react-bootstrap';
+import { Button, Carousel } from 'react-bootstrap';
 import library1 from './img/library1.jpg';
-
+import AddForm from "./AddForm";
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -24,23 +24,43 @@ class BestBooks extends React.Component {
     let url = `${process.env.REACT_APP_SERVER}/books`;
     try {
       const response = await axios.get(url);
-
       this.setState({
         books: response.data
       }, () => console.log(this.state.books));
     } catch (err) { console.log(err) }
   }
 
+  postBooks = async (newBook) => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/books`;
+      const response = await axios.post(url, newBook)
+      console.log(response.data);
+      //Using elipses/spread to copy over old data and add new book into new array
+      this.setState({ books: [...this.state.books, response.data] })
+    }
+    catch (err) { console.error(err) }
+  }
+
+  deleteBooks = async (_id) => {
+    console.log(_id);
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/books/${_id}`;
+      await axios.delete(url);
+      let updatedBooks = this.state.books.filter(book => book._id !== _id);
+      this.setState({ books: updatedBooks });
+    }
+    catch (err) { console.error(err) }
+  }
   // Tester code
   // function ImageCycle() {
   //   const [index, setIndex] = useState(1);
-  
+
   //   const handleClick = () => {
   //     setIndex(index => index % 3 + 1);
   //   };
-  
+
   //   const imageUrl = `library${index}.jpg`;
-  
+
   //   return (
   //     <img src={imageUrl} onClick={handleClick} alt="Cycle" />
   //   );
@@ -54,8 +74,8 @@ class BestBooks extends React.Component {
 
     return (
       <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
+        <h2>My Library &amp; Bookshelf</h2>
+        <AddForm postBooks={this.postBooks}/>
         {this.state.books.length > 0 ? (
           <Carousel>
             {this.state.books.map((book, id) => {
@@ -65,17 +85,18 @@ class BestBooks extends React.Component {
                     className="d-block w-100"
                     src={library1}
                     alt="First slide"
-                  />
-                  <BookCarousel book={book} />
+                    />
+                  <BookCarousel book={book} deleteBooks = {this.deleteBooks} />
+                    
                 </Carousel.Item>
               )
             })}
+          
           </Carousel>
-
         ) : (
           <h3>No Books Found :(</h3>
         )}
-
+        <Button onClick={this.deleteBooks}>Delete Book</Button>
       </>
     )
   }
--- src/BookCarousel.js
+++ src/BookCarousel.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
+import {Button} from "react-bootstrap";
 
 class BookCarousel extends React.Component {
     render() {
@@ -7,11 +8,13 @@ class BookCarousel extends React.Component {
         return (
             <>
                 {/* <Carousel.Item key={this.props.book._id}> */}
-                    <Carousel.Caption>
-                        <h3>{this.props.book.title}</h3>
-                        <p>{this.props.book.description}</p>
-                        <p>{this.props.book.status}</p>
-                    </Carousel.Caption>
+                <Carousel.Caption>
+                    <h3>{this.props.book.title}</h3>
+                    <p>{this.props.book.genre}</p>
+                    <p>{this.props.book.description}</p>
+                    <p>{this.props.book.status}</p>
+                    <Button onClick={() => this.props.deleteBooks(this.props.book._id)} type = "submit">Delete Book</Button>
+                </Carousel.Caption>
                 {/* </Carousel.Item> */}
             </>
         )
